14446ef55a048596407b202dd3383a9aac94441e
ABHIJITH <aabhivishnu@gmail.com>
2023-11-16 09:45:08 +0530

    Netflix-Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchComponent } from './pages/search/search.component';
+import { MovieDetailsComponent } from './pages/movie-details/movie-details.component';
+import{HttpClientModule} from '@angular/common/http'
+import { MovieApiServiceService } from './service/movie-api-service.service';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    MovieDetailsComponent,
+   
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
+    
   ],
-  providers: [],
+  providers: [MovieApiServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
